1d00612fe29cbf95e66e51897af6cd63d3138758
cmcreators237 <cmcreators237@gmail.com>
2024-08-14 19:09:02 +0100

    refactor and fix all the bugs on the cart page . All is working good for now

--- user/src/scripts/checkout.js
+++ user/src/scripts/checkout.js
@@ -4,35 +4,35 @@ import { saveDateIntoDataBase } from "../../../../general/data.js";
 import { incrementCart } from "../../../../general/data.js";
 import { deletDataInDataBase } from "../../../../general/data.js";
 import { decrementCart } from "../../../../general/data.js";
-const delivery = document.getElementById('delivery');
-const totalAmount = document.querySelector('.totalAmount');
-const location = document.getElementById('location');
-const checkout__btn = document.querySelector('.checkout__btn');
-const successContainer = document.querySelector('.success-container');
-const checkout__main = document.querySelector('.checkout__main');
-const emptyCartPage = document.querySelector('.empty-cart');
-const checkout_successBtn = document.getElementById('checkout_successBtn');
-const checkout__noResult = document.querySelector('#checkout__no-result');
+const delivery = document.getElementById("delivery");
+const totalAmount = document.querySelector(".totalAmount");
+const location = document.getElementById("location");
+const checkout__btn = document.querySelector(".checkout__btn");
+const successContainer = document.querySelector(".success-container");
+const checkout__main = document.querySelector(".checkout__main");
+const emptyCartPage = document.querySelector(".empty-cart");
+const checkout_successBtn = document.getElementById("checkout_successBtn");
+const checkout__noResult = document.querySelector("#checkout__no-result");
 const activeId = localStorage.getItem("activeID");
 const customer_Id = Number(activeId);
 
 //check if there is no item in the cart, and show no message.
-async function checkIfCartIsEmpty(){
-    const dataResult = await fetchDataFromDataBase(
-        "cart",
-        "customer_Id",
-        customer_Id
-      );
-      if(dataResult.length ===0){
-    checkout__main.classList.add('hideCheckout');
-    emptyCartPage.classList.remove('hide-noresult');
-      }
+async function checkIfCartIsEmpty() {
+  const dataResult = await fetchDataFromDataBase(
+    "cart",
+    "customer_Id",
+    customer_Id
+  );
+  if (dataResult.length === 0) {
+    checkout__main.classList.add("hideCheckout");
+    emptyCartPage.classList.remove("hide-noresult");
+  }
 }
-checkIfCartIsEmpty()
+checkIfCartIsEmpty();
 //increment the  quantity value and price value;
 
 async function newOrderQauntityAndPrice() {
-    totalAmount.innerHTML = 'loading...'
+  totalAmount.innerHTML = "loading...";
   const dataResult = await fetchDataFromDataBase(
     "cart",
     "customer_Id",
@@ -42,79 +42,134 @@ async function newOrderQauntityAndPrice() {
   let totalQty = 0;
   dataResult.forEach(item => {
     sum += item.product_price;
-   totalAmount.innerHTML = `${formatAmout(sum)}`;
+    totalAmount.innerHTML = `${formatAmout(sum)}`;
     totalQty += item.product_quantity;
-   localStorage.setItem('totalAmount', sum);
-   localStorage.setItem('totalQty', totalQty);
-    
+    localStorage.setItem("totalAmount", sum);
+    localStorage.setItem("totalQty", totalQty);
   });
   createANewOrder();
 }
 //generate a unique order numbe
 
-function generateOrderNUmber(){
-    let  orderNumber = '#JK';
-    
-    for(let i = 0; i< 5; i++){
-        const randaomNumbers = Math.floor(Math.random()*10);
-        orderNumber+=randaomNumbers;
-    }
-    return orderNumber;
+function generateOrderNUmber() {
+  let orderNumber = "#JK";
+
+  for (let i = 0; i < 5; i++) {
+    const randaomNumbers = Math.floor(Math.random() * 10);
+    orderNumber += randaomNumbers;
+  }
+  return orderNumber;
 }
-
-
 
 // newOrderQauntityAndPrice();
 // create a new order function, when the user click the checkout button, create an order.
 async function createANewOrder() {
   const userData = await fetchDataFromDataBase("users", "id", customer_Id);
-  const totalQty = localStorage.getItem('totalQty');
-  const totalAmount = localStorage.getItem('totalAmount');
-  console.log('this is the total amount and qty', totalAmount, totalQty);
+  const totalQty = localStorage.getItem("totalQty");
+  const totalAmount = localStorage.getItem("totalAmount");
+  console.log("this is the total amount and qty", totalAmount, totalQty);
   const newOrderData = {
     customer_name: userData[0].name,
     customer__phone: userData[0].phone,
-    customer_id : customer_Id,
+    customer_id: customer_Id,
     amount: Number(totalAmount),
     quantity: Number(totalQty),
-    status : 'Pending',
-    location : location.value,
+    status: "Pending",
+    location: location.value,
     delvery_type: delivery.value,
-    order_number : generateOrderNUmber()
-
+    order_number: generateOrderNUmber()
   };
-  if(newOrderData.location === ''){
-   location.setAttribute('required', true);
-   return;
+  if (newOrderData.location === "") {
+    location.setAttribute("required", true);
+    return;
+  }else{
+    saveOrderDetails(newOrderData);
   }
- 
-  console.log(newOrderData);
-   
-  saveOrderDetails(newOrderData);
 
+ 
+  
 }
 
-
-checkout__btn.addEventListener('click', async function(e){
-     checkout__btn.innerHTML = 'Please wait...'
-     checkout__btn.disabled=true;
-   await newOrderQauntityAndPrice();
-  
-})
+checkout__btn.addEventListener("click", async function(e) {
+  checkout__btn.innerHTML = "Please wait...";
+  checkout__btn.disabled = true;
+  await newOrderQauntityAndPrice();
+});
 
 //save new order details in the database
 
-async function saveOrderDetails(saveInfo){
-    const dataResult = await saveDateIntoDataBase( saveInfo,'orders');
-    if(dataResult.length !==0){
-        checkout__btn.innerHTML = 'Place an order'
-        successContainer.classList.remove('hidePopup');
-        await deletDataInDataBase('cart', 'customer_Id', customer_Id);
-    }
+async function saveOrderDetails(saveInfo) {
+  const dataResult = await saveDateIntoDataBase(saveInfo, "orders");
+  if (dataResult.length !== 0) {
+    getAllOrdersItems(dataResult[0].id)
+    checkout__btn.innerHTML = "Place an order";
+    successContainer.classList.remove("hidePopup");
+     
+  setTimeout(async function(){
+    await deletDataInDataBase("cart", "customer_Id", customer_Id);
+  }, 5000);
+  }
 }
-checkout_successBtn.addEventListener('click', function(e){
+checkout_successBtn.addEventListener("click", function(e) {
+  successContainer.classList.add("hidePopup");
+  setTimeout(() => {
+    window.location.reload();
+  }, 1500);
+});
 
-})
+//new order function to  save the orders in the database;
+
+async function getAllOrdersItems(order_id){
+  const dataResult = await fetchDataFromDataBase('cart', 'customer_Id', customer_Id);
+  dataResult.forEach(item=>{
+    const orderData = {
+      quantity : item.product_quantity,
+      product_image : item.product_image,
+      product_name : item.product_name,
+       price : item.product_price,
+      user_id: item.customer_Id,
+      order_id : order_id,
+    }
+    saveOrderItemsDetails(orderData);
+  })
+
+}
+
+
+//save cart items in the order items table for reference to get it back letter.
+
+async function saveOrderItemsDetails(saveData){
+  const data = await saveDateIntoDataBase(saveData, 'order_items');
+  console.log('this is the save order datails here', data);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- user/src/scripts/orders.js
+++ user/src/scripts/orders.js
@@ -1,10 +1,6 @@
 import { fetchDataFromDataBase } from "../../../../general/data.js";
 import { formatAmout } from "../../../../general/data.js";
-import { saveDateIntoDataBase } from "../../../../general/data.js";
-import { incrementCart } from "../../../../general/data.js";
-import { deletDataInDataBase } from "../../../../general/data.js";
-import { decrementCart } from "../../../../general/data.js";
-const orders__wrapper = document.querySelector(".orders__wrapper");
+const orders__wrapper = document.querySelector(".order__table--container");
 const noResultContainer = document.querySelector(".empty-cart");
 const activeId = localStorage.getItem("activeID");
 const customer_Id = Number(activeId);
@@ -26,70 +22,64 @@ getAllOrders();
 //render the all orders to the web page.
 
 function renderOrders(orders) {
-  orders.forEach(order => {
+  orders.forEach((order, index) => {
     const time = order.created_at.split("T")[0];
 
-    orders__wrapper.innerHTML += ` <div class="order__items">
-       
-          <div class="order__header">
-            <P >
-              <span>Order Number:</span>
-              <span>${order.order_number}</span>
-            </P>
-            <P>
-              <span>Order date:</span>
-              <span>${order.time}</span>
-            </P>
-          </div>
-          <div class="order__content">
-            <div class="order__item--body">
-              <P>
-                <span>Quantity:</span>
-                <span >
-                ${order.quantity}</span>
-              </P>
-              <P>
-                <span>Price:</span>
-                <span >${formatAmout(order.amount)}</span>
-              </P>
-                <span class="status_text">Order Status:</span>
-                <span class="${changeOrderStatusClassName(order.status)}">${order.status}</span>
-              </P>
-            </div>
-            <div class="order__item--footer">
-              <div class="order__buttons">
-                <a href = '/user/pages/order-invoice.html?id=${order.id}' class="see-details">See more</a>
-                <button class="cancel-order">Cancel Order</button>
+    orders__wrapper.innerHTML += ` 
+      <div class="order__data--item">
+                <!-- Order details header -->
+                <div class="order__data--header">
+                  <li class="data__items">
+                    <span class="data__text">Order Number:</span>
+                    <span class="data__value">${order.order_number}</span>
+                  </li>
+                  <li class="data__items">
+                    <span class="data__text">Order date:</span>
+                    <span class="data__value">${time}</span>
+                  </li>
+                </div>
+                <!-- Order details body -->
+                <div class="order__data--body">
+                  <div>
+                    <li class="data__items">
+                      <span class="data__text">Quantity:</span>
+                      <span class="data__value">${order.quantity}</span>
+                    </li>
+                    <li class="data__items">
+                      <span class="data__text">Price:</span>
+                      <span class="data__value">${formatAmout(order.amount)}</span>
+                    </li>
+                    <li class="data__items">
+                      <span class="data__text">Order status:</span>
+                      <span class="${changeOrderStatusClassName(
+                        order.status
+                      )}">${order.status}</span>
+                    </li>
+                    <!-- Button to view order details -->
+                    <div class="view-more">
+                      <button class="primary-button view__order-details">
+                      <a href="/user/pages/order-invoice.html?id=${order.id}">View details</a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-       
-       </div>`;
+    
+              `;
   });
+
+
+
+
+
 }
 
-function changeOrderStatusClassName(status){
- if(status === 'Processing'){
-    return 'state processing'
- }
- else if(status === 'Delivery'){
-    return 'state deliver';
- }else{
-    return 'state';
- }
+function changeOrderStatusClassName(status) {
+  if (status === "Processing") {
+    return "state processing";
+  } else if (status === "Delivery") {
+    return "state deliver";
+  } else {
+    return "state";
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
